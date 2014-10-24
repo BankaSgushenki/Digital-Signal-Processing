544d158413fdc49f9e6f248e70a29a2b34147040
Kostya Sergeev <kostyasergeev93@gmail.com>
2014-10-24 18:22:06 +0300

    update

--- index.js
+++ index.js
@@ -6,18 +6,12 @@ var graph = angular.module('graph', []);
 
 graph.controller('graphController', function ($scope) {
     $scope.data = [];
+    $scope.fourierData = [];
     $scope.graphParameters = {
-      expression: 'sin(2x) + cos(x)',
+      expression: 'sin(2x) + cos(3x)',
       step: 0.1,
       right: 16,
       left: 0,
-    }
-
-    $scope.expression = 'sin(2x) + cos(x)';
-    $scope.step = 0.1;
-
-    for (var i = 0; i < 16; i += 0.5) {
-        $scope.data._append(i,math.eval($scope.expression, scope(i)));
     }
 
     var margin = {top: 20, right: 20, bottom: 30, left: 40},
@@ -48,32 +42,60 @@ graph.controller('graphController', function ($scope) {
       .x(function(d) { return x(d.x); })
       .y(function(d) { return y(d.y); });
 
-    function scope(x) {
-        return {x: x}
+    function scope(x, k) {
+        return {x: x, k : k}
+    }
+
+    function drawGraph(data) {
+        svg.selectAll("path").remove();
+        svg.selectAll("g").remove();
+  
+        x.domain(d3.extent(data, function(d) { return d.x; })).nice();
+        y.domain(d3.extent(data, function(d) { return d.y; })).nice();
+  
+        svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+  
+        svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        svg.append("path")
+          .datum(data)
+          .attr("class", "line")
+          .attr("d", line);
+    }
+
+    $scope.base = function() {
+      drawGraph($scope.data);
+    }
+
+    $scope.fourier = function(expression) {
+      $scope.fourierData = [];
+      for (var i = 0; i < 16; i += 0.1) {
+        $scope.fourierData._append(i,math.eval(expression, scope(i)));
+      }
+      $scope.fourierData = $scope.fourierData.map(function (entry) {
+        return {
+          x: entry.x,
+          y: math.eval('x*e^(-2*PI*i/16)',scope(entry.y, entry.x))
+        }
+      });
+      $scope.fourierData = $scope.fourierData.map(function (entry) {
+        return {
+          x: entry.x,
+          y: math.sqrt(entry.y.re*entry.y.re + entry.y.im*entry.y.im)
+        }
+      });
+      drawGraph($scope.fourierData);
     }
 
     $scope.$watch('graphParameters', function(newVal, oldVal){
       $scope.data = [];
       for (var i = Number($scope.graphParameters.left); i < Number($scope.graphParameters.right); i += Number($scope.graphParameters.step)) {
         $scope.data._append(i,math.eval($scope.graphParameters.expression, scope(i)));
-      }
-      svg.selectAll("path").remove();
-      svg.selectAll("g").remove();
-
-      x.domain(d3.extent($scope.data, function(d) { return d.x; })).nice();
-      y.domain(d3.extent($scope.data, function(d) { return d.y; })).nice();
-
-      svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-      svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-      svg.append("path")
-        .datum($scope.data)
-        .attr("class", "line")
-        .attr("d", line);
+      }     
+      drawGraph($scope.data)
     }, true);
 });
